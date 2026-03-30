a61b7222723656e3d55a48feeaf340ebd7422a14
Szymon Głogowski <67717194+DEMONLIVE13@users.noreply.github.com>
2026-03-30 12:35:09 +0200

    DODATKOWE ZADANIE Z ALGORYTMY I STRUKTURY DANYCH
    ZADANIE DODATKOWE

---
+++ ProblemC.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int longest_increasing_subsequence(const std::vector<int>& sequence) {
+    std::vector<int> tails;
+
+    for (int num : sequence) {
+        auto it = std::lower_bound(tails.begin(), tails.end(), num);
+        if (it == tails.end()) {
+            tails.push_back(num);
+        }
+        else {
+            *it = num;
+        }
+    }
+
+    return tails.size();
+}
+
+int main() {
+    int Z; // Liczba zestawów danych
+    std::cin >> Z;
+
+    while (Z--) {
+        int N; // Długość ciągu
+        std::cin >> N;
+
+        std::vector<int> sequence(N);
+        for (int i = 0; i < N; ++i) {
+            std::cin >> sequence[i];
+        }
+
+        std::cout << longest_increasing_subsequence(sequence) << std::endl;
+    }
+
+    return 0;
+}
+
+
